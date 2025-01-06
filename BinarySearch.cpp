59ead767fa81bb0a7fe93cf149012c91a36c16e4
muhamadyassar <irenzuy@gmail.com>
2025-01-06 17:05:44 +0700

    Add files via upload

---
+++ BinarySearch.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Binary Search Implementation
+int binarySearch(int arr[], int left, int right, int target) {
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if(arr[mid] == target) {
+            return mid;
+        }
+        
+        if(arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+	cout<<"====================="<<endl;
+	cout<<"Muhammad yassar"<<endl;
+	cout<<"     231011402540
+	cout<<"====================="<<endl;
+    int arr[] = {2, 3, 4, 10, 40, 50, 60, 70};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+    
+    cout << "Using Binary Search:" << endl;
+    int result = binarySearch(arr, 0, n-1, target);
+    if(result == -1) {
+        cout << "Element not found" << endl;
+    } else {
+        cout << "Element found at index: " << result << endl;
+    }
+    
+    return 0;
+}
